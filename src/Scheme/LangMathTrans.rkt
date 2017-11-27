405123fa086c4e643955eeed721fbb78f1066990
David Rogers <droger7@students.towson.edu>
2017-11-27 14:53:15 -0500

    Final Commit

--- src/Scheme/LangMathTrans.rkt
+++ src/Scheme/LangMathTrans.rkt
@@ -4,19 +4,31 @@
 (define english '(zero one two three four five six seven eight nine ten))
 
 (define (go alist)
-  (display "Translation: ") (display(construct alist)) (newline)
-  (display "Addition: ") (display(add-between (construct alist) "+")) (display " = ") (display (foldl + 0 (construct alist))) (newline)
-  (display "Multiplication: ") (display(add-between (construct alist) "*")) (display " = ") (display (foldl * 1 (construct alist))) (newline)(newline)
+  (display "Translation: ") (display(trans alist)) (newline)
+  (display "Addition: ") (display(add-between (trans alist) "+")) (display " = ") (display (sum (trans alist))) (newline)
+  (display "Multiplication: ") (display(add-between (trans alist) "*")) (display " = ") (display (mult (trans alist))) (newline)(newline)
   )
 
-(define (construct alist)
-  (cond ((null? alist)'())
-        ((member (car alist) chinese)(cons (index-of chinese(car alist)) (construct(cdr alist))))
-        ((member (car alist) english)(cons (index-of english(car alist)) (construct(cdr alist))))
-        (else (construct(cdr alist)))
+(define (trans alist)
+  (cond ((null? alist) null)
+        ((member? (car alist) english)(cons (index-of english(car alist)) (trans(cdr alist))))
+        ((member? (car alist) chinese)(cons (index-of chinese(car alist)) (trans(cdr alist))))
+        (else (trans(cdr alist)))
         )
   )
 
-(go '(yi josh three si))
-(go '(yi nine josh six ba))
+(define (member? item alist)
+  (sequence-ormap (lambda (x)
+                    (equal? item x))
+                     alist))
 
+(define sum
+  (lambda (the-list)
+    (apply + the-list)))
+
+(define mult
+  (lambda (the-list)
+    (apply * the-list)))
+
+(go '(yi nine josh six josh ba))
+(go '(yi josh three si))
